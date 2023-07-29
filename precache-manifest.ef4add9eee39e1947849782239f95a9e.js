da14fbebf68cdeb204daa0a6f8b1a28f78b0789b
Surya Satria Hidayat <suryasatriahx@gmail.com>
2023-07-29 23:18:03 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.ef4add9eee39e1947849782239f95a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "baf5588581354982f00c",
+    "url": "/static/css/main.e0d37f7f.chunk.css"
+  },
+  {
+    "revision": "baf5588581354982f00c",
+    "url": "/static/js/main.baf55885.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "057d4251b96ccf34bd04d641a709b950",
+    "url": "/index.html"
+  }
+];
